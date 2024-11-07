2e24ed0373c52b4382f1eb1e2eee92b8cb5d36e6
ovh <oskarvoldbakkenhesle@gmail.com>
2024-11-07 15:20:40 +0000

    added config

--- database/prodtest_config.php
+++ database/prodtest_config.php
@@ -92,6 +92,17 @@ if ( ! defined( 'WP_DEBUG' ) ) {
 }
 
 define( 'DISABLE_WP_CRON', 'true' );
+
+
+define( 'WP_ALLOW_MULTISITE', true );
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', false );
+$base = '/';
+define( 'DOMAIN_CURRENT_SITE', 'multhx-25047.prodtest-osl.servebolt.cloud' );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
+
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
